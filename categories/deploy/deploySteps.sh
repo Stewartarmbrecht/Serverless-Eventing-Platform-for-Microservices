d7e426e18ec7acb7714f6dde61db9f541a47776c
Stewart Armbrecht <stewartarmbrecht@outlook.com>
2018-07-15 23:13:23 -0500

    Still fixing VSTS build on Linux.

--- categories/deploy/deploySteps.sh
+++ categories/deploy/deploySteps.sh
@@ -9,20 +9,20 @@ cd "${0%/*}"
 # Categories Microservice Deploy
 
 sh ./deploy-microservice.sh \
---resourceGroupName=$1"-categories" \
+--resourceGroupName="$1-categories" \
 --region="westus2" \
 --deploymentFile="./microservice.json" \
 --deploymentParameters="uniqueResourceNamePrefix,$1|bigHugeThesaurusApiKey,$2" \
---apiName=$1"-categories-api" \
+--apiName=≈"$1-categories-api" \
 --apiFilePath="./ContentReactor.Categories.Api.zip" \
---workerName=$1"-categories-worker" \
+--workerName="$1-categories-worker" \
 --workerFilePath="./ContentReactor.Categories.WorkerApi.zip" \
---dbAccountName=$1"-categories-db" \
+--dbAccountName="$1-categories-db" \
 --dbName="Categories" \
 --dbCollectionNames="Categories" \
 --dbPartitionKey="/userId" \
 --storageAccountName="" \
 --storageCollectionNames="" \
---eventResourceGroup=$1"-events" \
+--eventResourceGroup="$1-events" \
 --eventSubscriptionDeploymentFile="./eventGridSubscriptions-categories.json" \
 --eventSubscriptionParameters="uniqueResourceNamePrefix,$1"
