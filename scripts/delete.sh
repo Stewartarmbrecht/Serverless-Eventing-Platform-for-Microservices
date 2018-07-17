33bb858d45a9ba4f8dd31be742d4f49444749d22
Stewart Armbrecht <stewartarmbrecht@outlook.com>
2018-07-17 02:46:22 -0500

    adding a delete script to help with removing an entire instance.

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,29 +1,25 @@
 #!/bin/sh
 
-set -e
-set -u
-
 cd "${0%/*}"
 
 
-D() { D '\033[1;35m'`date +%Y-%m-%d-%H:%M:%S` $1'\033[0m'; }
-
-D "Logging In"
-az login
+echo "Run az login and az account set --subecription X if you have not already."
+# D "Logging In"
+# az login
 # D "Setting subcription id"
 # az account set --subscription $subscriptionId 
 
-D "Deleting Events Resource Group"
-az group delete -n $uniquePrefixString"-events" --no-wait -y
-D "Deleting categories Resource Group"
-az group delete -n $uniquePrefixString"-categories" --no-wait -y
-D "Deleting Audio Resource Group"
-az group delete -n $uniquePrefixString"-audio" --no-wait -y
-D "Deleting Text Resource Group"
-az group delete -n $uniquePrefixString"-text" --no-wait -y
-D "Deleting Images Resource Group"
-az group delete -n $uniquePrefixString"-images" --no-wait -y
-D "Deleting Proxy Resource Group"
-az group delete -n $uniquePrefixString"-proxy" --no-wait -y
-D "Deleting Web Resource Group"
-az group delete -n $uniquePrefixString"-web" --no-wait -y
+echo "Deleting Events Resource Group"
+az group delete -n "$1-events" --no-wait -y
+echo "Deleting categories Resource Group"
+az group delete -n "$1-categories" --no-wait -y
+echo "Deleting Audio Resource Group"
+az group delete -n "$1-audio" --no-wait -y
+echo "Deleting Text Resource Group"
+az group delete -n "$1-text" --no-wait -y
+echo "Deleting Images Resource Group"
+az group delete -n "$1-images" --no-wait -y
+echo "Deleting Proxy Resource Group"
+az group delete -n "$1-proxy" --no-wait -y
+echo "Deleting Web Resource Group"
+az group delete -n "$1-web" --no-wait -y
