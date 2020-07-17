226191458723ecf8acf43e339a03425f71fce8ba
Stewart Armbrecht <stewart@boundbybetter.com>
2020-07-17 06:12:27 -0500

    Added Deploy-EdenServiceInfrastructure with tests.

--- powershell/Eden/public/Deploy-EdenServiceInfrastructure.ps1
+++ powershell/Eden/public/Deploy-EdenServiceInfrastructure.ps1
@@ -1,35 +1,18 @@
 function Deploy-EdenServiceInfrastructure {
     [CmdletBinding()]
-    param()
+    param(  
+    )
     
     try {
-        $solutionName = Get-SolutionName
-        $serviceName = Get-ServiceName
-        
-        Set-EdenServiceEnvVariables -Check
+        $edenEnvConfig = Get-EdenEnvConfig -Check
     
-        $instanceName = Get-EnvironmentVariable "$solutionName.$serviceName.InstanceName"
-        $region = Get-EnvironmentVariable "$solutionName.$serviceName.Region"
-    
-        $loggingPrefix = "$solutionName $serviceName Deploy Infrastructure $instanceName"
-    
-        $resourceGroupName = "$instanceName-$serviceName".ToLower()
-        $deploymentFile = "./Infrastructure/Infrastructure.json"
-    
+        $loggingPrefix = "$($edenEnvConfig.SolutionName) $($edenEnvConfig.ServiceName) Deploy Infrastructure $($edenEnvConfig.EnvironmentName)"
+
         Write-BuildInfo "Deploying the service infrastructure." $loggingPrefix
-    
-        Connect-HostingEnvironment $loggingPrefix
-    
-        Write-BuildInfo "Creating the resource group: $resourceGroupName." $loggingPrefix
-        New-AzResourceGroup -Name $resourceGroupName -Location $region -Force | Write-Verbose
-    
-        Write-BuildInfo "Executing the deployment using: $deploymentFile." $loggingPrefix
-        New-AzResourceGroupDeployment `
-            -ResourceGroupName $resourceGroupName `
-            -TemplateFile $deploymentFile `
-            -TemplateParameterObject @{ InstanceName = $instanceName } | Write-Verbose
-    
-        Write-BuildInfo "Deployed the service infrastructure." $loggingPrefix
+
+        Invoke-EdenCommand "Deploy-ServiceInfrastructure" $edenEnvConfig $loggingPrefix
+        
+        Write-BuildInfo "Finished deploying the service infrastructure." $loggingPrefix
     }
     catch {
         Write-BuildError "Error deploying the service infrastructure. Message: '$($_.Exception.Message)'" $loggingPrefix
---
+++ powershell/tests/public/Deploy-EdenServiceInfrastructure.tests.ps1
@@ -0,0 +1,48 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+
+#since we match the srs/tests organization this works
+$here = $here -replace 'tests', 'Eden'
+
+. "$here\$sut"    
+
+# Import our module to use InModuleScope
+Import-Module (Join-Path $PSScriptRoot "../../Eden/Eden.psm1") -Force
+Import-Module (Join-Path $PSScriptRoot "../TestUtilities.psm1") -Force
+
+InModuleScope "Eden" {
+    Describe "Public/Deploy-EdenServiceInfrastructure" {
+        BeforeEach {
+            Mock Get-SolutionName { "TestSolution" }
+            Mock Get-ServiceName { "TestService" }
+            Set-TestEnvironment
+            [System.Collections.ArrayList]$log = @()
+            Mock Write-BuildInfo (Get-BuildInfoErrorBlock $log)
+            Mock Write-BuildError (Get-BuildInfoErrorBlock $log)
+        }
+        Context "When executed once successfully" {
+            It "Print the following logs" {
+                Mock Invoke-EdenCommand (Get-InvokeEdenCommandBlock $log)
+                Deploy-EdenServiceInfrastructure -Verbose
+                Assert-Logs $log @(
+                    "TestSolution TestService Deploy Infrastructure TestEnvironment Deploying the service infrastructure.",
+                    "TestSolution TestService Deploy Infrastructure TestEnvironment Deploy-ServiceInfrastructure TestSolution TestService",
+                    "TestSolution TestService Deploy Infrastructure TestEnvironment Finished deploying the service infrastructure."
+                )
+            }
+        }
+        Context "When executed with exception thrown" {
+            It "Prints the following logs" {
+                Mock Invoke-EdenCommand (Get-InvokeEdenCommandBlockWithError $log)
+                {
+                    Deploy-EdenServiceInfrastructure -Verbose
+                } | Should -Throw                
+                Assert-Logs $log @(
+                    "TestSolution TestService Deploy Infrastructure TestEnvironment Deploying the service infrastructure.",
+                    "TestSolution TestService Deploy Infrastructure TestEnvironment Deploy-ServiceInfrastructure TestSolution TestService",
+                    "TestSolution TestService Deploy Infrastructure TestEnvironment Error deploying the service infrastructure. Message: 'My Error!'"
+                )
+            }
+        }
+    }
+}
