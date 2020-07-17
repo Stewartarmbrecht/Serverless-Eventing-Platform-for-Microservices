abcaf207d2d11aef81d88a74610e67efc6cbfde0
Stewart Armbrecht <stewart@boundbybetter.com>
2020-07-17 06:13:31 -0500

    Added Deploy-EdenServiceApplication and tests.

--- powershell/Eden/public/Deploy-EdenServiceApplication.ps1
+++ powershell/Eden/public/Deploy-EdenServiceApplication.ps1
@@ -3,54 +3,41 @@ function Deploy-EdenServiceApplication {
     param()
     
     try {
-        $solutionName = Get-SolutionName
-        $serviceName = Get-ServiceName
-        
-        Set-EdenServiceEnvVariables -Check
+        $edenEnvConfig = Get-EdenEnvConfig -Check
     
-        $instanceName = Get-EnvironmentVariable "$solutionName.$serviceName.InstanceName"
-        $tenantId = Get-EnvironmentVariable "$solutionName.$serviceName.TenantId"
-        $region = Get-EnvironmentVariable "$solutionName.$serviceName.Region"
-    
-        $loggingPrefix = "$solutionName $serviceName Deploy App $instanceName"
-    
-        Write-BuildInfo "Deploying the applications." $loggingPrefix
-    
-        Connect-HostingEnvironment $loggingPrefix
-    
-        $automatedTestJob = Test-Automated `
-            -SolutionName $solutionName `
-            -ServiceName $serviceName `
-            -AutomatedUrl "https://$apiName-staging.azurewebsites.net/api/" `
-            -LoggingPrefix $loggingPrefix
-        
-        While($automatedTestJob.State -eq "Running")
-        {
-            $automatedTestJob | Receive-Job | Write-Verbose
-        }
-        
-        $automatedTestJob | Receive-Job | Write-Verbose
-        
-        if ($automatedTestJob.State -eq "Failed") {
-            Write-BuildError "The staging end to end testing failed." $loggingPrefix
-            Write-BuildError "Exiting deployment." $loggingPrefix
-            Get-Job | Remove-Job
-            throw "Automated tests failed."
-        }
-        
-        Get-Job | Remove-Job
+        $loggingPrefix = "$($edenEnvConfig.SolutionName) $($edenEnvConfig.ServiceName) Deploy Application $($edenEnvConfig.EnvironmentName)"
 
-        Invoke-StagingSwap `
-            -InstanceName $instanceName `
-            -ServiceName $serviceName `
-            -LoggingPrefix $loggingPrefix
-    
+        Write-BuildInfo "Deploying the application." $loggingPrefix
+
+        Write-BuildInfo "Connecting to the hosting environment." $loggingPrefix
+
+        Invoke-EdenCommand "Connect-ServiceHostingEnvironment" $edenEnvConfig $loggingPrefix
+        
+        Write-BuildInfo "Connected to the hosting environment." $loggingPrefix
+
+        Write-BuildInfo "Deploying the service application to staging." $loggingPrefix
+
+        Invoke-EdenCommand "Deploy-ServiceAppStaging" $edenEnvConfig $loggingPrefix
+        
+        Write-BuildInfo "Finished deploying the service application to staging." $loggingPrefix
+
+        Write-BuildInfo "Testing the staging instance of the service application." $loggingPrefix
+
+        Invoke-EdenCommand "Test-ServiceAppStaging" $edenEnvConfig $loggingPrefix
+        
+        Write-BuildInfo "Finished testing the staging instance of the service application." $loggingPrefix
+
+        Write-BuildInfo "Swapping the staging instance of the service application with the production instance." $loggingPrefix
+
+        Invoke-EdenCommand "Invoke-ServiceStagingSwap" $edenEnvConfig $loggingPrefix
+        
+        Write-BuildInfo "Finished swapping the staging instance of the service application with the production instance." $loggingPrefix
+
         Write-BuildInfo "Finished deploying the applications." $loggingPrefix
     }
     catch 
     {
-        Get-Job | Stop-Job | Remove-Job
-        Set-Location $currentDirectory
-        throw $_    
+        Write-BuildError "Error deploying the service application. Message: '$($_.Exception.Message)'" $loggingPrefix
+        throw $_
     }    
 }
---
+++ powershell/tests/public/Deploy-EdenServiceApplication.tests.ps1
@@ -0,0 +1,60 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+
+#since we match the srs/tests organization this works
+$here = $here -replace 'tests', 'Eden'
+
+. "$here\$sut"    
+
+# Import our module to use InModuleScope
+Import-Module (Join-Path $PSScriptRoot "../../Eden/Eden.psm1") -Force
+Import-Module (Join-Path $PSScriptRoot "../TestUtilities.psm1") -Force
+
+InModuleScope "Eden" {
+    Describe "Public/Deploy-EdenServiceApplication" {
+        BeforeEach {
+            Mock Get-SolutionName { "TestSolution" }
+            Mock Get-ServiceName { "TestService" }
+            Set-TestEnvironment
+            [System.Collections.ArrayList]$log = @()
+            Mock Write-BuildInfo (Get-BuildInfoErrorBlock $log)
+            Mock Write-BuildError (Get-BuildInfoErrorBlock $log)
+        }
+        Context "When executed once successfully" {
+            It "Print the following logs" {
+                Mock Invoke-EdenCommand (Get-InvokeEdenCommandBlock $log)
+                Deploy-EdenServiceApplication -Verbose
+                Assert-Logs $log @(
+                    "TestSolution TestService Deploy Application TestEnvironment Deploying the application.",
+                    "TestSolution TestService Deploy Application TestEnvironment Connecting to the hosting environment.",
+                    "TestSolution TestService Deploy Application TestEnvironment Connect-ServiceHostingEnvironment TestSolution TestService",
+                    "TestSolution TestService Deploy Application TestEnvironment Connected to the hosting environment.",
+                    "TestSolution TestService Deploy Application TestEnvironment Deploying the service application to staging.",
+                    "TestSolution TestService Deploy Application TestEnvironment Deploy-ServiceAppStaging TestSolution TestService",
+                    "TestSolution TestService Deploy Application TestEnvironment Finished deploying the service application to staging.",
+                    "TestSolution TestService Deploy Application TestEnvironment Testing the staging instance of the service application.",
+                    "TestSolution TestService Deploy Application TestEnvironment Test-ServiceAppStaging TestSolution TestService",
+                    "TestSolution TestService Deploy Application TestEnvironment Finished testing the staging instance of the service application.",
+                    "TestSolution TestService Deploy Application TestEnvironment Swapping the staging instance of the service application with the production instance.",    
+                    "TestSolution TestService Deploy Application TestEnvironment Invoke-ServiceStagingSwap TestSolution TestService",
+                    "TestSolution TestService Deploy Application TestEnvironment Finished swapping the staging instance of the service application with the production instance.",
+                    "TestSolution TestService Deploy Application TestEnvironment Finished deploying the applications."
+                )
+            }
+        }
+        Context "When executed with exception thrown" {
+            It "Prints the following logs" {
+                Mock Invoke-EdenCommand (Get-InvokeEdenCommandBlockWithError $log)
+                {
+                    Deploy-EdenServiceApplication -Verbose
+                } | Should -Throw                
+                Assert-Logs $log @(
+                    "TestSolution TestService Deploy Application TestEnvironment Deploying the application.",
+                    "TestSolution TestService Deploy Application TestEnvironment Connecting to the hosting environment.",
+                    "TestSolution TestService Deploy Application TestEnvironment Connect-ServiceHostingEnvironment TestSolution TestService",
+                    "TestSolution TestService Deploy Application TestEnvironment Error deploying the service application. Message: 'My Error!'"
+                )
+            }
+        }
+    }
+}
