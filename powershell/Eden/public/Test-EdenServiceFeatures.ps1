f72157f458d8c7af31699e01510f7918f60ff182
Stewart Armbrecht <stewart@boundbybetter.com>
2020-07-21 05:25:07 -0500

    Added Staging option to Test-EdenServiceFeatures

--- powershell/Eden/public/Test-EdenServiceFeatures.ps1
+++ powershell/Eden/public/Test-EdenServiceFeatures.ps1
@@ -5,7 +5,18 @@ function Test-EdenServiceFeatures
         [Parameter()]
         [Switch]$Continuous,
         [Parameter()]
-        [Switch]$BuildOnce
+        [Switch]$BuildOnce,
+        [Parameter()]
+        [Switch]$Staging
     )
-    Start-EdenServiceLocal -Continuous:($Continuous -and !$BuildOnce) -RunFeatureTests:(!$Continuous) -RunFeatureTestsContinuously:($Continuous)
+
+    if ($Staging) {
+        $edenEnvConfig = Get-EdenEnvConfig -Check
+        $loggingPrefix = "$($edenEnvConfig.SolutionName) $($edenEnvConfig.ServiceName) Test Staging $($edenEnvConfig.EnvironmentName)"
+        Write-EdenBuildInfo "Testing the staging service features." $loggingPrefix
+        Invoke-EdenCommand "Test-ServiceFeaturesStaging" $edenEnvConfig $loggingPrefix
+        Write-EdenBuildInfo "Finished testing the staging service features." $loggingPrefix
+    } else {
+        Start-EdenServiceLocal -Continuous:($Continuous -and !$BuildOnce) -RunFeatureTests:(!$Continuous) -RunFeatureTestsContinuously:($Continuous)
+    }
 }
