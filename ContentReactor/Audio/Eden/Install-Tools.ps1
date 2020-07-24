541853bea7d8f70314832b11c68f338d031e42d5
Stewart Armbrecht <stewart@boundbybetter.com>
2020-07-24 14:19:38 +0000

    Added live-server and markserv tools for the Audio project.  These tools run webservers that are used to render local html and markdown fles in a browser.

--- ContentReactor/Audio/Eden/Install-Tools.ps1
+++ ContentReactor/Audio/Eden/Install-Tools.ps1
@@ -77,6 +77,12 @@ try {
         Write-EdenBuildInfo "Installing Allure to generate test results html reports." $LoggingPrefix
         npm install -g allure-commandline
 
+        Write-EdenBuildInfo "Installing Live Server to run a web server for accessing html files." $LoggingPrefix
+        npm install -g live-server
+
+        Write-EdenBuildInfo "Installing Markserve to use for serving up markdown documentation." $LoggingPrefix
+        npm i -g markserv
+
     }
 } catch {
     $message = $_.Exception.Message
